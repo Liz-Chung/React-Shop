9b6aae0a63d89704baf8f45b51e18ff97c2fe5cd
Liz-Chung <lizzhongx@gmail.com>
2024-05-22 20:57:24 +0900

    no message

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { memo } from "react";
 import Error from "../views/Error";
 import Index from "../views/Index";
+import Main from '../pages/Main/MainPage';
+import Product from '../pages/Product/ProductPage';
 
 const Router = (): JSX.Element => {
   return (
     <Routes>
       <Route path="*" element={<Error />} />
       <Route path="/" element={<Index />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/products" element={<Product />} />
       {/* 라우팅 추가 해보세요. */}
     </Routes>
   );
