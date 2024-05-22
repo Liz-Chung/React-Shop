5e2b76a2dc98fed5c91d81fa9689fea95c461755
Liz-Chung <lizzhongx@gmail.com>
2024-05-22 17:05:12 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import Drawer from "./components/common/Drawer";
 import Router from "./router/router";
 import Nav from "./components/common/Nav";
-import Footer from "./components/common/Footer";
+import Footer from "./components/common/Footer/Footer";
 
 const App = (): JSX.Element => {
 
