530c57cb10264c7d009224d27c090935458682cf
Liz-Chung <lizzhongx@gmail.com>
2024-05-22 18:06:51 +0900

    no message

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -48,10 +48,10 @@ const Nav = (): JSX.Element => {
               className="px-3 py-2 rounded-md border dark:bg-gray-700 dark:border-gray-600"
             />
             <Link to="/cart" className="relative">
-              <span className={isDarkMode ? `${styles.icon} ${styles.iconDark}` : styles.icon}>
+              <span className="material-symbols-outlined h-6 w-6 text-gray-800 dark:text-white">
                 shopping_bag
               </span>
-              <span className={styles.badge}>
+              <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full transform translate-x-1/2 -translate-y-1/2">
                 {cartItemCount}
               </span>
             </Link>
