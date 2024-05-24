09fd1d4c4f8dc58e93299e17afe3a39ca987f788
Liz-Chung <lizzhongx@gmail.com>
2024-05-24 17:08:17 +0900

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    base: '/',
     plugins: [react()],
     resolve: {
       alias: {
