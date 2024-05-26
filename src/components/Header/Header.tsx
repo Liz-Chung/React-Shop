2f140b38fcc55f4e4fd1121c9470a185a4dbbaad
Liz-Chung <lizzhongx@gmail.com>
2024-05-26 18:42:19 +0900

    no message

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -71,7 +71,7 @@ export default function Header(props: PropsType) {
               {themeDark ? 'light_mode' : 'dark_mode'}
             </span>
           </button>
-          <div className={styles.searchBox}>
+          <div className={`${styles.searchBox} ${themeDark ? styles.searchBoxDark : styles.searchBoxLight}`}>
             <SearchProduct searchToggle={searchToggle} setSearchToggle={setSearchToggle} />
           </div>
           <div className={`${styles.label} ${styles.commonHoverEffect}`} onClick={() => navigate('/cart')}>
